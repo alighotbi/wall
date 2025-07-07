1dd5d62965f1b6efea14f89f975aa347e5f22ad2
Ali Ghotbi <mraligh95@gmail.com>
2025-07-07 12:24:44 +0330

    Added ad creation and ad editing

--- ads/urls.py
+++ ads/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import AdlistView
+from .views import AdlistView, AdCreateView, AdDetailView
 
 app_name = 'ads'
 
 urlpatterns = [
-    path('list/', AdlistView.as_view(), name='adlist'),
+    path('list/', AdlistView.as_view(), name='ad-list'),
+    path('create/', AdCreateView.as_view(), name='ad-create'),
+    path('detail/<int:pk>/', AdDetailView.as_view(), name='ad-detail'),
 ]
--- ads/views.py
+++ ads/views.py
@@ -3,6 +3,9 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.parsers import MultiPartParser
+
+from django.shortcuts import get_object_or_404
 
 from .models import Ad
 from .serializers import AdSerializer
@@ -17,3 +20,44 @@ class AdlistView(APIView, StandardPagination):
         result = self.paginate_queryset(ads, request)
         serializer = AdSerializer(result, many=True)
         return self.get_paginated_response(serializer.data)
+    
+    
+class AdCreateView(APIView):
+    serializer_class = AdSerializer
+    permission_classes = [IsAuthenticated]
+    parser_classes = [MultiPartParser]
+    
+    def post(self, request):
+        serializer = AdSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.validated_data['publisher'] = request.user
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
+class AdDetailView(APIView):
+    serializer_class = AdSerializer
+    
+    def get(self, request, pk):
+        ad = Ad.objects.get(id=pk)
+        serializer = AdSerializer(ad)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    
+class AdModifyView(APIView):
+    serializer_class = AdSerializer
+    permission_classes = [IsAuthenticated]
+    parser_classes = [MultiPartParser]
+    
+    def put(self, request, pk):
+        ad = Ad.objects.get(publisher=request.user, id=pk)
+        serializer = AdSerializer(ad, data=request.data ,partial=True)
+        if serializer.is_valid():
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        ad = Ad.objects.get(publisher=request.user, id=pk)
+        ad.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
